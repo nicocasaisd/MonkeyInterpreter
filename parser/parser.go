5acbc35f49176621a5989da6c85946ecfd14041f
nicocasaisd <nicolas.casais.dassie@gmail.com>
2026-09-08 17:38:19 -0300

    add parser func to parse return and test return statement

--- parser/parser.go
+++ parser/parser.go
@@ -60,9 +60,23 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+	// TODO: we're skipping expressions until we find a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
 
 func (p *Parser) parseLetStatement() ast.Statement {
